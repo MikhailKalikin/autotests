9b597506f30623ee24b901314c9012cbb3d36841
mikhailkalikin <mkalikin@mail.ru>
2023-10-16 08:40:43 +0400

    some added files

--- src/main/java/com/example/lab/ApplicationController.java
+++ src/main/java/com/example/lab/ApplicationController.java
@@ -1,34 +1,64 @@
 package com.example.lab;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
 
 
 @RestController
 @RequestMapping("/result")
 public class ApplicationController {
 
-
     @PostMapping("/plus")
     public String plus(String number1, String number2, int base) {
         Calculate calculate = new Calculate();
+        saveCalculationResult(number1, number2, calculate.plus(number1, number2, base), "plus" );
         return calculate.plus(number1, number2, base);
+
+
     }
 
-    /*@PostMapping("minus")
-    public String minus() {
-        return description;
+
+
+
+    @PostMapping("/minus")
+    public String minus(String number1, String number2, int base) {
+        Calculate calculate = new Calculate();
+        return calculate.minus(number1, number2, base);
     }
 
-    @PostMapping("multiply")
-    public String multiply() {
-        return description;
+
+    @PostMapping("/multiply")
+    public String multiply(String number1, String number2, int base) {
+        Calculate calculate = new Calculate();
+        return calculate.multiply(number1, number2, base);
     }
 
-    @PostMapping("divide")
-    public String divide() {
-        return description;
-    }*/
+
+    @PostMapping("/divide")
+    public String divide(String number1, String number2, int base) {
+        Calculate calculate = new Calculate();
+        return calculate.divide(number1, number2, base);
+    }
+
+    private final CalculationResultRepository resultRepository;
+
+    @Autowired
+    public ApplicationController(CalculationResultRepository resultRepository) {
+        this .resultRepository = resultRepository;
+    }
+
+    private void saveCalculationResult(String number1, String number2, String result, String operation) {
+        CalculationResult calculationResult = new CalculationResult();
+        calculationResult.setOperand1(number1);
+        calculationResult.setOperand2(number2);
+        calculationResult.setResult(result);
+        calculationResult.setOperation(operation);
+        calculationResult.setExecutionTime(LocalDateTime.now());
+        resultRepository.save(calculationResult);
+    }
+
 }
---
+++ src/main/java/com/example/lab/CalculationResult.java
@@ -0,0 +1,70 @@
+package com.example.lab;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class CalculationResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String operand1;
+    private String operand2;
+    private String result;
+    private String operation;
+    private LocalDateTime executionTime;
+
+    // Геттеры и сеттеры для полей
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(String operand1) {
+        this.operand1 = operand1;
+    }
+
+    public String getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(String operand2) {
+        this.operand2 = operand2;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public LocalDateTime getExecutionTime() {
+        return executionTime;
+    }
+
+    public void setExecutionTime(LocalDateTime executionTime) {
+        this.executionTime = executionTime;
+    }
+}
